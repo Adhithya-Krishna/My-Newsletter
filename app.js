291eacec3a8e71be8ed44bc91e3c6f0b30058644
Adhithya Krishna S <adithyakrshn4@gmail.com>
2023-05-02 15:49:37 +0530

    PORT Update

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/failure.html",(req,res)=>{
     res.redirect("/");
 });
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000...")
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Server is running on port "+process.env.PORT+"...")
 })
 
